fdba7b52071676d9b898924fdfe37f79e2c850d9
Juniven Saavedra <junivensaavedra@gmail.com>
2025-10-05 03:34:51 +0800

    Add query to display all transactions where profit is negative

--- queries.sql
+++ queries.sql
@@ -4,14 +4,18 @@ use finance;
 show tables;
 
 
+# Basic query to view all data in the table
 select *
 from tbl_sales;
 
 
 #1 List all records of sales for Canada.
+# Filters all rows where the country column equals 'Canada'
+# Returns all columns for Canadian sales transactions
 select *
 from tbl_sales
 where country = 'Canada';
+
 
 #2 Retrieve the top 10 highest sales transactions.
 select *
@@ -19,9 +23,11 @@ from tbl_sales
 order by sales desc
 limit 10;
 
+
 #3 Show all unique product names.
 select distinct product
 from tbl_sales;
+
 
 #4 Find sales made in January 2024.
 select *
@@ -32,3 +38,9 @@ where Month_Name = 'January'
 select *
 from tbl_sales
 where Month_Number = 1;
+
+
+#5 Display all transactions where profit is negative.
+select *
+from tbl_sales
+where profit < 0;
